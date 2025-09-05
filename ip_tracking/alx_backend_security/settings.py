7026582bd90d3a666e4b8319ddfa2c951f7b56fa
Barry-owino <barryowino5@gmail.com>
2025-09-05 16:15:22 +0300

    setting up the app and the sttings.py

--- ip_tracking/alx_backend_security/settings.py
+++ ip_tracking/alx_backend_security/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tracking',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'tracking.middleware.IPTrackingMiddleware',
 ]
 
 ROOT_URLCONF = 'alx_backend_security.urls'
