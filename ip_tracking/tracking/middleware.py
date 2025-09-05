cebeecf662cbf6c06844ede2511baec3b2db7e11
Barry-owino <barryowino5@gmail.com>
2025-09-05 21:57:04 +0300

    updated the middleware for BlockedIP address

--- ip_tracking/tracking/middleware.py
+++ ip_tracking/tracking/middleware.py
@@ -1,5 +1,17 @@
-from .models import RequestLog
+from django.http import HttpResponseForbidden
+from .models import RequestLog, BlockedIP
 import datetime
+
+class BlockedIPMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        ip = request.META.get("REMOTE_ADDR", "")
+        #check if ip is in blocklist
+        if BlockedIP.objects.filter(ip_address=ip).exit():
+            return HttpsResponseForbidden("Your IP has been blocked.")
+        return self.get_response(request)
 
 class IPTrackingMiddleware:
     def __init__(self, get_response):
