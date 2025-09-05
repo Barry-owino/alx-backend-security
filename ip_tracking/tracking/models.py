7f0922fef57e7d25173f7a16e0209b0642674753
Barry-owino <barryowino5@gmail.com>
2025-09-05 21:26:04 +0300

    updated the model with BlockedIP class and run migrations on the database

--- ip_tracking/tracking/models.py
+++ ip_tracking/tracking/models.py
@@ -9,3 +9,10 @@ class RequestLog(models.Model):
     def __str__(self):
         return f"{self.ip_address} - {self.path} at {self.timestamp}"
 
+class BlockedIP(models.Model):
+    ip_address = models.GenericIPAddressField(unique=True)
+    added_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Blocked: {self.ip_address}"
+
