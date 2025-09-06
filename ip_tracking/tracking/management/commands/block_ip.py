f3529912d7d238885547f3bfd39ff3542c00db39
Barry-owino <barryowino5@gmail.com>
2025-09-06 17:05:30 +0300

    fixed import error

--- ip_tracking/tracking/management/commands/block_ip.py
+++ ip_tracking/tracking/management/commands/block_ip.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from tracking.models import blockedIP
+from tracking.models import BlockedIP
 
 class Command(BaseCommand):
     help = "Add an IP address to the BlockedIP list"
